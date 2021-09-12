c34f2f327b4f6890010a94dc98990a03cb56c99f
bet-liujie <vliujie@ahau.edu.cn>
2021-09-12 19:27:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	DlByHand/Begin/DateOperate.py

--- DlByHand/Begin/LinearNeuralNetwork.py
+++ DlByHand/Begin/LinearNeuralNetwork.py
@@ -58,6 +58,7 @@ def normal(x, mu, sigma):
 x = np.arange(-7, 7, 0.01)  # 可视化
 
 params = [(0, 1), (0, 2), (3, 1)]
-d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x',
-         ylabel='p(x)', figsize=(4.5, 2.5),
-         legend=[f'mean{mu},std{sigma}' for mu, sigma in params])  # 可视化,但没出来不知道为啥
+a = d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x',
+             ylabel='p(x)', figsize=(4.5, 2.5),
+             legend=[f'mean{mu},std{sigma}' for mu, sigma in params])  # 可视化,但没出来不知道为啥
+a.show()
