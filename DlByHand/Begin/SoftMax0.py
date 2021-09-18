a4e813b1bc1d65dd2e134c70b1002627383d58d9
bet-liujie <vliujie@ahau.edu.cn>
2021-09-18 18:42:07 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	DlByHand/Begin/DateOperate.py

--- DlByHand/Begin/SoftMax0.py
+++ DlByHand/Begin/SoftMax0.py
@@ -1,6 +1,7 @@
 import torch
 from IPython import display
 from d2l import torch as d2l
+import matplotlib.pyplot as plt
 
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
---
+++ DlByHand/Begin/softmax-easy.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+# 初始化模型参数
+net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+
+net.apply(init_weights);
+loss = nn.CrossEntropyLoss()
+
+# 优化算法
+
+trainer = torch.optim.SGD(net.parameters(), lr=0.1)
+
+# 训练
+num_epochs = 10
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+plt.show()
